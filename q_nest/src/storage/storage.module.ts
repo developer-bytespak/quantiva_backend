67c6d777d74c8e99a600b3a4bf706317d23b215c
ghalibzaidi110 <ghalibzaidi115@gmail.com>
2026-02-03 02:32:23 +0500

    cloudnry setup

--- q_nest/src/storage/storage.module.ts
+++ q_nest/src/storage/storage.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { StorageService } from './storage.service';
+import { CloudinaryService } from './cloudinary.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [StorageService],
-  exports: [StorageService],
+  providers: [StorageService, CloudinaryService],
+  exports: [StorageService, CloudinaryService],
 })
 export class StorageModule {}
 
