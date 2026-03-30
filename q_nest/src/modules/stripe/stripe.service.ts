11fd4cc437f358d57d5e1b6f02d2ddb8543d62f7
developer-bytespak <developers@bytesplatform.com>
2026-03-30 10:06:31 -0700

    updat

--- q_nest/src/modules/stripe/stripe.service.ts
+++ q_nest/src/modules/stripe/stripe.service.ts
@@ -9,9 +9,9 @@ export class StripeService {
     const secret = process.env.STRIPE_SECRET_KEY;
     if (!secret) {
       throw new Error('STRIPE_SECRET_KEY is required');
-    }
+    } 
     this.stripe = new Stripe(secret, {
-      apiVersion: '2026-01-28.clover',
+     
     });
   }
 
